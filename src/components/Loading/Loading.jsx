78ea83232bf066a0cd8d6b787e5ded302098b217
Swajeet9597 <swajeetdharmadhikari@gmail.com>
2025-01-04 17:23:13 +0530

    uppp

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -3,10 +3,15 @@ import React from 'react';
 const Loading = () => {
   return (
     <div>
-    <div class="cssload-loading">
-	<i></i>
-	<i></i>
-	<i></i>
+<div id="floatingCirclesG">
+	<div class="f_circleG" id="frotateG_01"></div>
+	<div class="f_circleG" id="frotateG_02"></div>
+	<div class="f_circleG" id="frotateG_03"></div>
+	<div class="f_circleG" id="frotateG_04"></div>
+	<div class="f_circleG" id="frotateG_05"></div>
+	<div class="f_circleG" id="frotateG_06"></div>
+	<div class="f_circleG" id="frotateG_07"></div>
+	<div class="f_circleG" id="frotateG_08"></div>
 </div>
     </div>
   );
